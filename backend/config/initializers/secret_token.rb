66c3342ff3caa6e4e3a3925694fe1144618d7caa
Harris <sltsim@gmail.com>
2011-12-03 13:47:01 -0800

    template for backend

---
+++ backend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backend::Application.config.secret_token = '2ad3aed5abaa2378cbd3edd4190c373093ab3325f1408be9f88aa0ef987f4d0309c19465b6ad22d75728cc8b8aa2c185328fbcd87826967eb7683e5cfda2861a'
