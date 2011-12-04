d5f4afb8606fd81e5ea0dd8ab1cc1368bdd904ca
Ricky Wong <rickwong@gmail.com>
2011-12-03 20:07:58 -0800

    UI

--- backend/app/controllers/connections_controller.rb
+++ backend/app/controllers/connections_controller.rb
@@ -1,10 +1,11 @@
 class ConnectionsController < ApplicationController
   def index
-    all_names =  $client.connections.all.map do |connection| 
-      "#{connection.first_name} #{connection.last_name}"
-    end
-    render :text => all_names.join(",")
-  end
-  
+    @connections = $client.connections.all.select {|item| !item.pictureUrl.nil?}
 
+    # @all_names =  $client.connections.all.map do |connection| 
+    #  "#{connection.first_name} #{connection.last_name}"
+    #end
+    # render :text => @all_names.join(",")
+    # render :connections
+  end
 end
