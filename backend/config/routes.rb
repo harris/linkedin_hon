d2db0cd140b3b8d251cb7b855f31b1b20d64a1c0
Harris <sltsim@gmail.com>
2011-12-03 16:25:49 -0800

    remove index page

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,18 +1,4 @@
 Backend::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
   resource :session do 
     member do 
       get 'callback'
@@ -20,4 +6,5 @@ Backend::Application.routes.draw do
   end
   
   resources :connections
+  root :to => 'connections#index'
 end
