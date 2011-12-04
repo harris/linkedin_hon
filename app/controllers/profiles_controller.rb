7e12ef86e4c03162f088d20f7f31e322efe0efa3
Ricky Wong <rickwong@gmail.com>
2011-12-04 15:30:22 -0800

    ux

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-  
+
   def index
     last_connection = Connection.order("created_at DESC").find(:first)
     random_ids = 10.times.map{ 1 + rand(last_connection.id) }    
