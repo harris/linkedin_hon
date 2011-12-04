1eab2661161ad4c7ff949786cb27239e73d1be07
Harris <sltsim@gmail.com>
2011-12-04 12:34:53 -0800

    fix anchor and score

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,6 +1,6 @@
 class ScoresController < ApplicationController
   def create    
-    connection = Connection.find_by_linkedin_id(params[:linkedin_id])
+    connection = Connection.find_by_linkedin_id(params[:linkedin_id][1..-1])
     connection.rated_by(session[:user_id], params[:score].to_i)
     render :nothing => true
   end
