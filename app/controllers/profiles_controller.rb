dd08172c106d64dbf9a5927ad21d563edb36fea4
Harris <sltsim@gmail.com>
2011-12-04 16:28:24 -0800

    show your own score

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,12 @@
 class ProfilesController < ApplicationController
-
+  before_filter :login_required, :only => :show
   def index
     last_connection = Connection.order("created_at DESC").find(:first)
     random_ids = 10.times.map{ 1 + rand(last_connection.id) }    
     @connections = Connection.where(:id => random_ids)
+  end
+  
+  def show
+    @connection =  current_user.connection
   end
 end
